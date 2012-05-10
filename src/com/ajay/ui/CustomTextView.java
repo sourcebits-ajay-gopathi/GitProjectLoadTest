99290fe2ffa79e82c71ab0d9e57673efc6c5ae6a
ajay <ajay@ajay.(none)>
2012-05-10 12:39:58 +0530

    pkg added

---
+++ src/com/ajay/ui/CustomTextView.java
@@ -0,0 +1,13 @@
+package com.ajay.ui;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView{
+
+	public CustomTextView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
