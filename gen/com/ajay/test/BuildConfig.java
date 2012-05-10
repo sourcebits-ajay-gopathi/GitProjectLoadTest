32589a891c79be78b1ea3017f7a550241dc6b608
ajay <ajay@ajay.(none)>
2012-05-10 12:34:52 +0530

    first po

---
+++ gen/com/ajay/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ajay.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ajay/test/GitProjectPushActivity.java
@@ -0,0 +1,13 @@
+package com.ajay.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GitProjectPushActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
